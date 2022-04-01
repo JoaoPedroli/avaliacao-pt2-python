a4829b350421c68f45a6e8bd6c1f6b79f27a4fc5
Joao010 <joaopedrocz12345@gmail.com>
2022-04-01 15:13:15 -0300

    update

--- Q4.py
+++ Q4.py
@@ -9,8 +9,9 @@ tax_cres_B = 1.5
 anos = 0
 while pop_pais_A < pop_pais_B:
   anos += 1
-  pop_pais_A += (pop_pais_A / (tax_cres_A * 100))
-  pop_pais_B += (pop_pais_B / (tax_cres_B * 100))
+  pop_pais_A += (pop_pais_A * (tax_cres_A / 100))
+  pop_pais_B += (pop_pais_B * (tax_cres_B / 100))
+  print(pop_pais_A, pop_pais_B)
 
 print(f'''Levaria {anos} anos para que a populacao do pais A ultrapasse ou iguale a populacao do pais B,
 mantidas as taxas de crescimento.''')
---
+++ Q7.py
@@ -0,0 +1,10 @@
+sal_inicial = int(input('Digite o salario inicial do funcionario: '))
+sal_atual = sal_inicial
+por_inicial = 1.5
+
+for i in range(1996, 2023):
+  print(sal_atual)
+  sal_atual += (sal_inicial * (por_inicial / 100))
+  por_inicial *= 2
+
+print(sal_inicial)
